059302391353d1def079589041b94fe56a1d20d6
ShengDaolin <15900859362@163.com>
2021-06-25 16:04:19 +0100

    Initial commit

---
+++ src/main/java/com/floow/challege/web/FloowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.floow.challege.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloowProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloowProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/floow/challege/web/FloowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floow.challege.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloowProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
